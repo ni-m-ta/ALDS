d403049f15f94da02fde364814c0c4c9f820738c
ni-m-ta <tatsuyanisato@gmail.com>
2022-12-14 13:13:35 -0800

    done

---
+++ ALDS1_5_A_ExhaustiveSearch.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+anses = []
+
+n = int(input())
+a = list(map(int,input().split())).sort
+q = int(input())
+m = list(map(int,input().split()))
+al = []
+
+for j
+    for i in range(2**n): 
+
+
+for i in range(q):
+    y = m[i]
+    x = 0
+
+    ans = 'no'
+    while 
+    print(ans)
+
